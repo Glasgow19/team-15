0126f662b58b155df384fcb613bdf2cf139c181f
Gosia Borowiec <m.g.borowiec@sms.ed.ac.uk>
2019-10-26 18:37:46 +0100

    Edited articles

---
+++ src/main/java/com/dresscode/controllers/ArticlesController.java
@@ -0,0 +1,15 @@
+package com.dresscode.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ArticlesController {
+	
+	@GetMapping("/articles") 
+	public String articles() {
+		return "articles";
+	}
+
+	
+}
