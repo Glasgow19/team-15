7bcefb9bae933e64e27486602e12203d90aa6b70
unknown <sharma.vrinda2001@gmail.com>
2019-10-26 23:22:40 +0100

    Quiz

---
+++ src/main/java/com/dresscode/Quiz.java
@@ -0,0 +1,42 @@
+package com.dresscode;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@RestController
+public class Quiz {
+	@Autowired
+	ResourceLoader resourceLoader;
+	
+	@RequestMapping("/quiz")
+	public List<Question> readQuestions() {
+		List<Question> questions = new ArrayList<Question>();
+		try {
+			byte[] content = resourceLoader.getResource("classpath:data/questions.json").getInputStream().readAllBytes();
+			ObjectMapper mapper = new ObjectMapper();
+			questions =Arrays.asList(mapper.readValue(content, Question[].class));
+			
+			for(int i = 0 ; i < questions.size() ; i++) {
+				System.out.println(questions.get(i).getQuestion());
+				List<String> s = questions.get(i).getAnswers();
+				for(int j= 0 ; j<s.size() ; j++)
+				System.out.println(s.get(j)) ;
+			}	
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return questions;
+	}
+}
