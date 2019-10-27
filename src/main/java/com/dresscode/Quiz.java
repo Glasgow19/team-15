6bdaa5f68bbe6be20b365997581ec32a44ed03b6
unknown <sharma.vrinda2001@gmail.com>
2019-10-27 01:22:43 +0000

    #Quiz1

--- src/main/java/com/dresscode/Quiz.java
+++ src/main/java/com/dresscode/Quiz.java
@@ -1,15 +1,12 @@
 package com.dresscode;
 
 import java.io.*;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ResourceLoader;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -18,26 +15,24 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 public class Quiz {
 	@Autowired
 	ResourceLoader resourceLoader;
-	
-	@RequestMapping("/quiz")
-	public List<Question> readQuestions() 
+	public int count ; 
+	public Quiz(){
+		count = -1 ;
+	}
+	public Question readQuestions() 
 	{
 		List<Question> questions = new ArrayList<Question>();
 		try {
-			byte[] content = resourceLoader.getResource("classpath:data/questions.json").getInputStream().readAllBytes();
+			//byte[] content = resourceLoader.getResource("classpath:data/questions.json").getInputStream().readAllBytes();
+			ClassLoader classloader = Thread.currentThread().getContextClassLoader();
+			byte[] content = classloader.getResourceAsStream("data/questions.json").readAllBytes();
 			ObjectMapper mapper = new ObjectMapper();
 			questions =Arrays.asList(mapper.readValue(content, Question[].class));
-			
-			for(int i = 0 ; i < questions.size() ; i++) {
-				System.out.println(questions.get(i).getQuestion());
-				List<String> s = questions.get(i).getAnswers();
-				for(int j= 0 ; j<s.size() ; j++)
-				System.out.println(s.get(j)) ;
-			}	
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}
-		return questions;
+		}count++ ;
+		return questions.get(count) ;
+		
 	}
 }
--- src/main/java/com/dresscode/controllers/QuizesController.java
+++ src/main/java/com/dresscode/controllers/QuizesController.java
@@ -1,20 +1,36 @@
 package com.dresscode.controllers;
+import java.util.List;
 
+//import Quiz ;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import com.dresscode.Question;
+import com.dresscode.Quiz;
 
 @Controller
 public class QuizesController {
-	
 	@GetMapping("/quiz") 
-	public String Quiz() {
-		return "Quiz";
+	public String quiz(@RequestParam(name="Question", required=false, defaultValue="NULL") String Answers, Model model, Model model1, Model model2, Model model3) {
+		Quiz Q = new Quiz();
+		for(int i =0 ; i<5 ; i++) {
+		Question q ;
+		q = Q.readQuestions();
+		model.addAttribute("Question", q.getQuestion()) ;
+		List<String> s = q.getAnswers() ;
+		model1.addAttribute("Answers1", s.get(0)) ;
+		model2.addAttribute("Answers2", s.get(1)) ;
+		model3.addAttribute("Answers3", s.get(2)) ;
+		}
+		return "quiz";
 	}
 	
-	@PostMapping("/quizComp")
-	public String quizComp(String foo) {
-		System.out.println(foo);
+	@PostMapping("/quizComplete")
+	public String quizComp(String result) {
+		
+		
 		return "Quiz";
 	}
 	
